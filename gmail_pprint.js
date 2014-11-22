b34609fe5200b4bdd641cf408decdc6c36e5799b
Deepanjan Roy <deepanjan.roy@mail.mcgill.ca>
2014-11-22 12:42:32 -0500

    Delete old js file

--- gmail_pprint.js
+++
@@ -1,8 +0,0 @@
-msgs = document.getElementsByClassName("message");
-for(var i = 0; i < msgs.length; i++) {
-    msg = msgs[i];
-    msg_table = msg.children[0];
-    for (var j=1; j < msg_table.children.length -1; j++) {
-        msg_table.removeChild(msg_table.children[j]);
-    }
-}
