cb2447b483b78679357bdbe172589138f32cb6d6
ShivanKaul <shivan@omsignal.com>
2015-06-16 21:55:59 -0400

    Remove attachment images

--- extension/src/gmail_pprint.js
+++ extension/src/gmail_pprint.js
@@ -1,14 +1,25 @@
 // Get...
-// Delete header which right now is the first table. Future changes to the print view might break this.
+// Delete Gmail header, which right now is the first table.
 var tables = document.getElementsByTagName("table");
 // Delete receiver + sender cruft. The email id that shows up next to the name should be enough, methinks.
 var receiver_sender = document.getElementsByClassName("recipient");
+// Delete attachment images
+var attachments = document.getElementsByClassName("att");
+
 // Remove...
 tables[0].parentNode.removeChild(tables[0])
-for (var i = 0; i < receiver_sender.length; i++) {
-	// Delete all children (so sad)
-	while (receiver_sender[i].firstChild) {
-		receiver_sender[i].removeChild(receiver_sender[i].firstChild);
+deleteAll(receiver_sender)
+deleteAll(attachments)
+
+// At the end, force-open print dialog
+window.print()
+
+// HELPER FUNCTION: delete all instances
+function deleteAll(collection) {
+	for (var i = 0; i < collection.length; i++) {
+		// Delete all children (so sad)
+		while (collection[i].firstChild) {
+			collection[i].removeChild(collection[i].firstChild);
+		}
 	}
 }
-window.print()
